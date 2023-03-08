855b288210bdf266625db546e543ea0878162909
Davimrp <daviwendell2015@gmail.com>
2023-03-08 11:58:02 -0300

    Adicionadas as funcionalidades Javascript de adicionar e remover tarefas

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,8 +1,58 @@
 const botaoNovaTarefa = document.querySelector('.btn-nova-tarefa');
 const janelaNovaTarefa = document.querySelector('.nova-tarefa');
 const botaoSairDaNovaTarefa = document.querySelector('.img-sair');
+const botaoAdicionar = document.querySelector('#adicionar');
 
+let tarefas = [];
 
+function criaElementos(titulo, conteudo){
+    const divTarefa = document.createElement('div');
+    const divImgSairTrf = document.createElement('div');
+    const imgSairTrf = document.createElement('img');
+    const tituloElement = document.createElement('p');
+    const conteudoElement = document.createElement('p');
+
+    imgSairTrf.src =  'assets/imgs/sair.png'
+
+    divTarefa.appendChild(divImgSairTrf);
+    divTarefa.appendChild(tituloElement);
+    divTarefa.appendChild(conteudoElement);
+    divImgSairTrf.appendChild(imgSairTrf);
+
+    divTarefa.classList.add('tarefa');
+    divImgSairTrf.classList.add('img-sair-trf');
+    tituloElement.setAttribute('id:', 'titulo-trf');
+    conteudoElement.setAttribute('id:', 'conteudo-trf');
+    imgSairTrf.classList.add('img-excluir');
+
+    tituloElement.style.fontSize = '1.5rem';
+    conteudoElement.style.textAlign = 'start';
+    conteudoElement.style.fontSize = '1.1rem';
+    conteudoElement.style.marginLeft = '10px';
+
+    tituloElement.textContent = titulo;
+    conteudoElement.textContent = conteudo;
+
+    return divTarefa;
+}
+
+function adicionarNovaTarefa(){
+    const titulo = document.querySelector('#titulo').value;
+    const conteudo = document.querySelector('#conteudo').value;
+    const novaTarefa = criaElementos(titulo, conteudo);
+    tarefas.push({titulo, conteudo});
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    document.querySelector('.tarefas-feitas').appendChild(novaTarefa);
+    sairDaNovaTarefa();
+}
+
+botaoAdicionar.addEventListener('click', adicionarNovaTarefa);
+
+function abrirNovaTarefa(e){
+    janelaNovaTarefa.classList.remove('some')
+    document.querySelector('.nova-tarefa-background').classList.remove('some');
+}
+botaoNovaTarefa.addEventListener("click", abrirNovaTarefa)
 
 function sairDaNovaTarefa(){
     janelaNovaTarefa.classList.add('some')
@@ -11,8 +61,25 @@ function sairDaNovaTarefa(){
 
 botaoSairDaNovaTarefa.addEventListener('click', sairDaNovaTarefa)
 
-function abrirNovaTarefa(e){
-    janelaNovaTarefa.classList.remove('some')
-    document.querySelector('.nova-tarefa-background').classList.remove('some');
+// Carregar tarefas do localStorage ao iniciar a página
+if(localStorage.getItem('tarefas')) {
+    tarefas = JSON.parse(localStorage.getItem('tarefas'));
+    tarefas.forEach(tarefa => {
+        const novaTarefa = criaElementos(tarefa.titulo, tarefa.conteudo);
+        document.querySelector('.tarefas-feitas').appendChild(novaTarefa);
+    });
 }
-botaoNovaTarefa.addEventListener("click", abrirNovaTarefa)
+
+document.addEventListener('click', (e)=>{
+    if (e.target.classList == 'img-excluir'){
+        const btnExcluir = e.target
+        const divTarefa = btnExcluir.parentNode.parentNode
+        const index = Array.from(divTarefa.parentNode.children).indexOf(divTarefa)
+        
+        tarefas.splice(index, 1)
+        localStorage.setItem('tarefas', JSON.stringify(tarefas))
+
+        window.location.reload()
+    } 
+    
+})
